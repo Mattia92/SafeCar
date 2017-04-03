e487ba5035c73fce0acb7c36f76ef9608cd8c89b
apiro <albypiro@me.com>
2017-04-03 15:44:24 +0200

    swipe plugs

--- app/src/main/java/com/example/albertomariopirovano/safecar/settings/fragments/SettingsSmartObjectsFragment.java
+++ app/src/main/java/com/example/albertomariopirovano/safecar/settings/fragments/SettingsSmartObjectsFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +14,17 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.chauthai.swipereveallayout.ViewBinderHelper;
 import com.example.albertomariopirovano.safecar.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.albertomariopirovano.safecar.adapters.RecyclerAdapter;
 import com.podcopic.animationlib.library.AnimationType;
 import com.podcopic.animationlib.library.StartSmartAnimation;
 
@@ -31,10 +36,8 @@ public class SettingsSmartObjectsFragment extends Fragment {
 
     private TextView clickableSpeaker, clickablePlug;
     private TextView hiddenSpeaker;
-    private ListView hiddenPlug;
-
-    private List<String> plugList;
-    PlugListAdapter plugListAdapter;
+    private RecyclerView recyclerView;
+    private RecyclerAdapter plugAdapter;
 
     View v;
 
@@ -48,16 +51,15 @@ public class SettingsSmartObjectsFragment extends Fragment {
         clickableSpeaker = (TextView) v.findViewById(R.id.pairing_speakers);
         clickablePlug = (TextView) v.findViewById(R.id.gestione_plug);
         hiddenSpeaker = (TextView) v.findViewById(R.id.pairing_speakers_hidden);
-        hiddenPlug = (ListView) v.findViewById(R.id.gestione_plug_hidden);
-        plugList = new ArrayList<String>();
+        setupList(v);
+        //plugList = new ArrayList<String>();
 
         //TODO(Fetch plug for user)
-        for(int i = 0; i < 3; i++) {
-            plugList.add("Plug " + i);
-        }
+        // fatto nel metodo createList()
+        //for(int i = 0; i < 3; i++) {
+        //    plugList.add("Plug " + i);
+        //}
 
-        plugListAdapter = new PlugListAdapter();
-        hiddenPlug.setAdapter(plugListAdapter);
 
         clickableSpeaker.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,20 +80,38 @@ public class SettingsSmartObjectsFragment extends Fragment {
         clickablePlug.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(hiddenPlug.isShown()){
-                    slide_up(getActivity(), hiddenPlug);
-                    hiddenPlug.setVisibility(View.GONE);
+                if(recyclerView.isShown()){
+                    slide_up(getActivity(), recyclerView);
+                    recyclerView.setVisibility(View.GONE);
                 }
                 else{
-                    hiddenPlug.setVisibility(View.VISIBLE);
-                    slide_down(getActivity(), hiddenPlug);
+                    recyclerView.setVisibility(View.VISIBLE);
+                    slide_down(getActivity(), recyclerView);
                 }
             }
         });
         // hide until its title is clicked
-        hiddenPlug.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
 
         return v;
+    }
+
+    private void setupList(View v) {
+        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        plugAdapter = new RecyclerAdapter(getActivity(), createList(5));
+        recyclerView.setAdapter(plugAdapter);
+    }
+
+    private List<String> createList(int n) {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add("View " + i);
+        }
+
+        return list;
     }
 
     public static void slide_up(Context ctx, View v){
@@ -117,67 +137,4 @@ public class SettingsSmartObjectsFragment extends Fragment {
             }
         }
     }
-
-
-
-    class PlugListAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return plugList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return plugList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-
-            if (convertView == null) {
-                convertView = View.inflate(getActivity(), R.layout.plug_list, null);
-                new ViewHolder(convertView);
-            }
-            final ViewHolder holder = (ViewHolder) convertView.getTag();
-            holder.name.setText(plugList.get(position));
-            holder.del.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    StartSmartAnimation.startAnimation(holder.item, AnimationType.SlideOutRight, 1000, 0, true);
-                    plugList.remove(position);
-                    final Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            hiddenPlug.setAdapter(plugListAdapter);
-                        }
-                    }, 1000);
-                }
-            });
-
-            return convertView;
-        }
-
-
-        class ViewHolder {
-            private TextView name;
-            private Button del;
-            private View item;
-
-            public ViewHolder(View view) {
-                name = (TextView) view.findViewById(R.id.name);
-                del = (Button) view.findViewById(R.id.del);
-                item = view.findViewById(R.id.item);
-
-                view.setTag(this);
-            }
-        }
-    }
-
 }
